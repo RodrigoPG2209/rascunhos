0190a7936c1e39bd4856089ed23957d7470fa297
RodrigoPG2209 <wizzpg@gmail.com>
2016-12-15 15:11:25 -0200

    Update numeros_raiz_quadrada_inteiros.py
    Acerto da função

--- numeros_raiz_quadrada_inteiros.py
+++ numeros_raiz_quadrada_inteiros.py
@@ -1,53 +1,35 @@
 from math import sqrt
 
-def sqrt_int_noint(a=0, z=0):
-    if a > z != 0:
-		    print("Ao setar os valores iniciais e finais, o valor inicial deve ser menor ou igual ao valor final. A operação será ABORTADA.")
-    else:
-        print(" ")
-        print(" ")
-        print("Digite valores para descobrir quantos e quais sao os numeros que resultam em raiz quadrada exata.")
-        print(" ")
+def sqrt_int_noint():
+    print(" ")
+    print(" ")
+    print("Digite valores para descobrir quantos e quais sao os numeros que resultam em raiz quadrada exata.")
+    print(" ")
 
-        loop = 1
+    loop = 1
 
-        while loop == 1:
-            inicio = a
-            final = z
+    while loop == 1:
+        inicio = int(input("Digite um numero inteiro inicial: "))
+        final = int(input("Digite um numero inteiro final: "))
 
-            if a > 0: print("Parâmetro inicial já setado para:", a)
-            if z > 0: print("Parâmetro final já setado para:", z)
+        if final < inicio:
+            print("O valor inicial deve ser menor que o final. A operacao sera reiniciada.")
+            print("   ")
+        elif inicio < 0 or final < 0:
+            print("Tanto o valor inicial quanto o final devem ser maiores que zero.")
+            print("   ")
+        else:
+            break
 
-            if inicio == 0:
-                inicio = int(input("Digite um numero inteiro inicial: "))
+#                       x: valor a ser inserido na lista
+#                                                               for: instrução de loop. O início e final do loop é definido pelas variáveis 'inicio' e 'final' dentro de range()
+#                                                               for: o range não considera ir até o valor final, por isso é necessário somar
+#                                                                                                if: cláusula para inclusão de x à lista
+    valores_inteiros = [str(x) + " (" + str(int(sqrt(x))) + ")" for x in range(inicio, final +1) if x % sqrt(x) == 0]
+    valores_nao_inteiros = [x for x in range(inicio, final +1) if x not in valores_inteiros]
 
-            if final == 0:
-                final = int(input("Digite um numero inteiro final: "))
-
-            if final < inicio:
-                print("O valor inicial deve ser menor que o final. A operacao sera reiniciada.")
-                print(" ")
-
-                inicio = 0
-                final = 0
-
-            else:
-                break
-
-        # valores_inteiros = []
-        # valores_nao_inteiros = []
-
-#                           x: valor a ser inserido na lista
-#                             for: instrução de loop. O início e final do loop é definido pelas variáveis 'inicio' e 'final' dentro de range()
-#                             for: o range não considera ir até o valor final, por isso é necessário somar
-#                                                              if: cláusula para inclusão de x à lista
-        valores_inteiros = [x for x in range(inicio, final +1) if x % sqrt(x) == 0]
-        valores_nao_inteiros = [x for x in range(inicio, final +1) if x not in valores_inteiros]
-
-        print("Quantidade de valores cuja raiz quadrada resulta em numeros inteiros:", str(len(valores_inteiros)))
-        print("Quais sao:", valores_inteiros)
-        print(" ")
-        print("Quantidade de valores cuja raiz quadrada nao resulta em numeros inteiros:", str(len(valores_nao_inteiros)))
-        print("Quais sao:", valores_nao_inteiros)
-
-sqrt_int_noint
+    print("Quantidade de valores cuja raiz quadrada resulta em numeros inteiros:", str(len(valores_inteiros)))
+    print("Quais sao:", valores_inteiros)
+    print(" ")
+    print("Quantidade de valores cuja raiz quadrada nao resulta em numeros inteiros:", str(len(valores_nao_inteiros)))
+    print("Quais sao:", valores_nao_inteiros)
